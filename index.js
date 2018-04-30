04f545c046a0950dcf27c166c88c3dc6c0e688f3
Kim <szekim88@gmail.com>
2018-04-30 19:51:00 +0800

    push all file

---
+++ index.js
@@ -0,0 +1,148 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var http = require('http');
+
+var Schema = mongoose.Schema;
+const PORT = process.env.PORT || 5000
+
+
+export MONGOLAB_URI= "mongodb://szekim:kim12345678@kimcluster-shard-00-00-w4ocf.mongodb.net:27017,kimcluster-shard-00-01-w4ocf.mongodb.net:27017,kimcluster-shard-00-02-w4ocf.mongodb.net:27017/test?ssl=true&replicaSet=KimCluster-shard-0&authSource=admin"
+
+
+
+
+var dbURI = process.env.MONGOLAB_URI;
+
+//var dbURI = "mongodb://localhost/innobook"
+
+const options = {
+    reconnectTries: Number.MAX_VALUE,
+    poolSize: 10
+};
+
+mongoose.connect(dbURI).then(
+    () => {
+        console.log("Database connection established!");
+    },
+    err => {
+        console.log("Error connecting Database instance due to: ", err);
+    }
+);
+
+var registerSchema = mongoose.Schema({
+    pin: Number
+});
+var availablePinData = mongoose.model('availablepins', registerSchema)
+
+var registeredSchema = mongoose.Schema({
+    pin: Number,
+    uuid: String,
+    name: String
+})
+var registeredData = mongoose.model('registeredpins', registeredSchema)
+
+var app = express();
+app.use(bodyParser.json());
+
+
+http.createServer(app).listen(PORT);
+console.log("The server is start listening on port " +PORT);
+
+app.post('/register', function (req, res) {
+    var register = new availablePinData({
+        pin: req.body.pin,
+        uuid: req.body.uuid,
+        name: req.body.name
+    })
+    try {
+        availablePinData.find({
+            'pin': req.body.pin
+        }, function (err, result) {
+            if (result.length > 0) {
+                availablePinData.remove({
+                    'pin': req.body.pin
+                }, function (err, result) {
+                    console.log("remove")
+                });
+                var newRegister = new registeredData({
+                    pin: req.body.pin,
+                    uuid: req.body.uuid,
+                    name: req.body.name
+                });
+                newRegister.save(function (err) {
+                    console.log("saved")
+                    res.send("Pin valid. your device is now registered")
+                    res.end()
+                });
+            } else {
+                res.send("Invalid Pin")
+                res.end()
+            }
+        });
+    } catch (err) {
+        console.error(err)
+        res.send("Error:" + err)
+        res.end()
+    }
+});
+
+app.post('/addpin', function (req, res) {
+    try {
+        availablePinData.find({
+            'pin': req.body.pin
+        }, function (err, result) {
+            if (result.length > 0) {
+                res.send("Duplicate pin found")
+                res.end()
+            } else {
+                var newpin = new availablePinData({
+                    pin: req.body.pin
+                });
+                newpin.save(function (err) {
+                    console.log("New pin saved")
+                    res.send("New pin save")
+                    res.end()
+                })
+            }
+        });
+    } catch (err) {
+        console.error(err)
+        res.send("Error:" + err)
+        res.end()
+    }
+});
+
+app.get('/availablepin', function (req, res) {
+    try {
+        availablePinData.find({}, function (err, result) {
+            if (result.length > 0) {
+                res.send(result)
+                res.end()
+            } else {
+                res.send("No Available pin")
+            }
+        });
+    } catch (err) {
+        console.error(err)
+        res.send("Error:" + err)
+        res.end()
+    }
+});
+
+app.get('/registereddevice', function (req, res) {
+    try {
+        registeredData.find({}, function (err, result) {
+            if (result.length > 0) {
+                res.send(result)
+                res.end()
+            } else {
+                res.send("No registered data found")
+            }
+        });
+    } catch (err) {
+        console.error(err)
+        res.send("Error:" + err)
+        res.end()
+    }
+});
